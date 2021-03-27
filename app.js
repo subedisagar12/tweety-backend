a4e2ed8498e7cdd424af9b4250dc87224c3e3343
feensolutions <feensolutions@gmail.com>
2021-03-27 13:57:00 +0545

    checking cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import path from "path";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 let PORT = process.env.PORT || "5000";
 
 app.use(express.json());
@@ -20,15 +21,7 @@ import authenticate from "./middlewares/Authenticate.js";
 
 // User Routes
 // app.options("*", cors());
-// app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use("/profileImages", express.static("profileImages"));
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("client/tweety/build"));
