1409488a78ec8c5b7422429163fbf02aa2b62169
feensolutions <feensolutions@gmail.com>
2021-03-27 13:44:13 +0545

    checking cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 let PORT = process.env.PORT || "5000";
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 const corsOptions = {
   "Access-Control-Allow-Origin": "https://tweety-sagar.netlify.app",
 };
